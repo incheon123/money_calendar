821436bac3250ba6c899203705b076d0cc9680f2
incheon123 <xorgus704@gmail.com>
2024-04-30 00:58:50 +0900

    수정

--- src/main/java/com/example/money_management/entity/Room.java
+++ src/main/java/com/example/money_management/entity/Room.java
@@ -1,12 +1,17 @@
 package com.example.money_management.entity;
 
+import com.example.money_management.enumType.RoomType;
+import com.example.money_management.money_interface.Generable;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
@@ -20,7 +25,7 @@ import java.util.concurrent.ThreadLocalRandom;
 @Builder
 @Setter
 @Getter
-public class Room extends TimeLog{
+public class Room extends TimeLog implements Generable, Serializable{
     //방 아이디
     @Id
     @Column(name = "room_id")
@@ -55,14 +60,19 @@ public class Room extends TimeLog{
     @Column(name = "PASSWORD")
     private String password;
 
-    public void generateId(){
-        this.rid = ThreadLocalRandom.current().nextLong(4, Long.MAX_VALUE);
-    }
+    @Column(name = "ROOM_TYPE")
+    @Enumerated(EnumType.STRING)
+    private RoomType roomType;
+    
+    @Transient
+    private String requestRoomType;
 
     @PrePersist
     public void init(){
         this.currentPeopleCount = 1;
-        generateId();
+        this.rid = generateId();
         if(password != null) isSetPw = true;
+        if(requestRoomType.equals("CHATTING")) this.roomType = RoomType.CHATTING;
+        else this.roomType = RoomType.PRIVATE;
     }
 }
